564da1acee16b6d32cfb7820097f5b4d0e7bf191
Evy Kassirer <evy.kassirer@gmail.com>
2014-12-27 12:37:49 -0500

    better comments, cleaner code

--- content.js
+++ content.js
@@ -3,44 +3,59 @@
 // found in the LICENSE file.
 
 /**
+ * for debugging - turns on console log statements
+ * @type {boolean}
+ */
+var DEBUG = false;
+
+/**
  * Global variable containing a dictionary of a css attribute and the
  * style element where the style is set accross the page.
+ * @type {Object.<string, string>}
  */
 var allStyles = {};
+// set default values
+allStyles['line-height'] = "0";
+allStyles['letter-spacing'] = "0";
+allStyles['word-spacing'] = "0";
+allStyles['font-weight'] = "0";
+allStyles['font-style'] = "0";
 
 /**
  * Creates the HTML style element that sets global styles for the page
  * when the page is opened.
+ *
+ * @param {string} attribute to be set
+ * @param {string} new value of the attribute
  */
-function loadInitialValue(attribute, result) {
+var loadInitialValue = function(attribute, result) {
   var style = document.createElement('style');
   style.type = 'text/css';
   // This means it was set to default.
-  if (!result || !result[attribute] || result[attribute] == 0) 
-      style.innerHTML = '';
+  if (!result || !result[attribute] || result[attribute] == "0")
+    style.innerHTML = '';
   else {
     style.innerHTML = '* { ' + attribute + ': '+ result[attribute] + '; }"';
-      console.log(attribute + " is loaded as: " + result[attribute]);
+    if (DEBUG) console.log(attribute + " is loaded as: " + result[attribute]);
   }
   document.head.appendChild(style);
   allStyles[attribute] = style;
 }
 
 /**
- * Fetches the current stored styles.
+ * Fetch the current stored styles.
  * If there is no result (no stored style) loadInitialValue interprets this
  * as default style.
+ * @param {string} attribute to be fetched
  */
-chrome.storage.sync.get('line-height', function(result) { 
-  loadInitialValue('line-height', result); });
-chrome.storage.sync.get('letter-spacing', function(result) { 
-  loadInitialValue('letter-spacing', result); });
-chrome.storage.sync.get('word-spacing', function(result) { 
-  loadInitialValue('word-spacing', result); });  
-chrome.storage.sync.get('font-weight', function(result) { 
-  loadInitialValue('font-weight', result); });  
-chrome.storage.sync.get('font-style', function(result) { 
-  loadInitialValue('font-style', result); });  
+var fetchStyle = function(attribute) {
+  chrome.storage.sync.get(attribute, function(result) {
+  loadInitialValue(attribute, result); });
+}
+// Fetch each style.
+for (var key in allStyles) {
+  fetchStyle(key);
+}
 
 /**
  * When the style is changed from the popup menu, the html style tag is
@@ -53,7 +68,7 @@ chrome.storage.onChanged.addListener(function(changes, namespace) {
     var storageChange = changes[key];
     // If the value is 0, that means return to default, so remove
     // any added styling.
-    if (storageChange.newValue == 0) {
+    if (storageChange.newValue == "0") {
       allStyles[key].innerHTML = '';
       continue;
     }
